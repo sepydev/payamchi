f22e46e7f663b3c36481496a8512989b23d75274
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-05-13 20:20:34 +0430

    Create message receiver and define label model
    Make migrations of models

--- payamchi/core/models/base_model.py
+++ payamchi/core/models/base_model.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-
-
-
 class BaseModel(models.Model):
     is_active = models.BooleanField(verbose_name=_('فعال'), default=True)
     create_date = models.DateTimeField(verbose_name=_('تاریخ ایجاد'), auto_now_add=True)
