ce4706c75b6c50429381a2d25b0c25d9d55dd166
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-05-19 00:08:27 +0430

    Create and design base layout

--- payamchi/core/urls.py
+++ payamchi/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import HomeView
+from .views.home import HomeView
+from .views.inbox import InboxView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('inbox/', InboxView.as_view(), name='inbox'),
 
 ]
